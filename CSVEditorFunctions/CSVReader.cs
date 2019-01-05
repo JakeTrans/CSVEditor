595c7b057f61b570f37697dbc9495bb013bc1a36
JakeTrans <Jake_Damner@Yahoo.co.uk>
2019-01-05 13:56:10 +0000

    csv reader added

---
+++ CSVEditorFunctions/CSVReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CSVEditorFunctions
+{
+    public class CSVReader
+    {
+
+        public string CurrentFile { get; set; }
+
+        DataTable CSVDT;
+
+        public void ReadFile(string FilePath)
+        {
+
+
+        }
+
+        public void WriteFile(string filepath)
+        {
+
+        }
+
+        private void TranslateCSVtoTable()
+        {
+            CSVDT = new DataTable();
+        }
+
+    }
+}
